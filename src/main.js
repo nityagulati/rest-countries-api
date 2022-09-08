920b5056dc39f1cecb59b179d65d23701f4e588e
Gulati <Nitya.Gulati@surescripts.com>
2022-09-08 01:59:09 -0400

    import font awesome icons

--- src/main.js
+++ src/main.js
@@ -1,6 +1,23 @@
+/*jshint -W033 */
+
 import { createApp } from 'vue'
 import App from './App.vue'
 
+/* import the fontawesome core */
+import { library } from '@fortawesome/fontawesome-svg-core'
+
+/* import font awesome icon component */
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+/* import specific icons */
+import { faMoon } from '@fortawesome/free-regular-svg-icons'
+import { faLightbulb } from '@fortawesome/free-regular-svg-icons'
+
+/* add icons to the library */
+library.add(faMoon, faLightbulb)
+
 import './assets/main.css'
 
-createApp(App).mount('#app')
+createApp(App)
+.component('font-awesome-icon', FontAwesomeIcon)
+.mount('#app')
