28c6e88b0e8ab3185ffccafcb2e301879c304a33
Nitya Gulati <46540503+nityagulati@users.noreply.github.com>
2022-09-10 22:27:59 -0400

    add theme switcher

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -8,8 +8,7 @@
         aria-live="polite"
         @click.stop="themeToggle"
     >
-        <font-awesome-icon v-if="!isDarkTheme" icon="fa-regular fa-moon" />
-        <font-awesome-icon v-else icon="fa-regular fa-lightbulb" />
+        <font-awesome-icon :icon="!darkMode ? 'fa-regular fa-moon' : 'fa-regular fa-lightbulb'"></font-awesome-icon>
         {{toggleText}}
     </button>
 </header>
@@ -20,17 +19,20 @@
 export default {
     data() {
         return {
-            isDarkTheme: false,
+            darkMode: false,
             toggleText: 'Dark Mode'
         }
     },
     methods: {
         themeToggle() {
-            this.isDarkTheme = !this.isDarkTheme
-            if(this.isDarkTheme) {
+            let htmlElement = document.documentElement
+            this.darkMode = !this.darkMode
+            if(this.darkMode) {
                 this.toggleText = 'Light Mode'
+                htmlElement.setAttribute('theme', 'dark')
             } else {
                 this.toggleText = 'Dark Mode'
+                htmlElement.setAttribute('theme', 'light')
             }
         }
     }
@@ -55,6 +57,7 @@ h1 {
 
 .theme-toggle {
     cursor: pointer;
+    color: var(--color-text);
     font-weight: var(--font-weight-semi);
     border: none;
     background: none;
